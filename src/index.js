047101d7dcdfc06148ea8691e1fed100133c7f3b
John Jason Taladro <john.jason.taladro@gmail.com>
2022-05-05 11:19:18 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+
+const renderReactDom = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+  renderReactDom();
+}
